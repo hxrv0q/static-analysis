2b408542e16cdf64bd3fe8d5d3b77a49f5eb3d09
Ivan Tkachuk <116971836+seshotake@users.noreply.github.com>
2023-09-28 14:46:34 +0300

    Refactor and clean lab1

--- src/lab1.py
+++ src/lab1.py
@@ -1,120 +1,51 @@
-# %%
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
 
-class SampleDistribution:
-    def __init__(self, data):
-        self.data = data
-
-        self.min = np.min(data)
-        self.max = np.max(data)
-        self.range = self.max - self.min
-        self.n = len(data)
-
-        self.mean = np.mean(data)
-        self.var = np.var(data)
-        self.std = np.std(data)
-        self.skew = stats.skew(data)
-        self.kurtosis = stats.kurtosis(data)
-
-    def plot(self, bins: int):
-        plt.figure(figsize=(20, 5))
-
-        plt.subplot(1, 4, 1)
-        self.histogram(bins)
-
-        plt.subplot(1, 4, 2)
-        self.polygon(bins, f"Полігон m={bins}")
-
-        plt.subplot(1, 4, 3)
-        self.polygon(
-            bins, cumulative=True, title=f"Полігон накопичених частот m={bins}"
-        )
-
-        plt.subplot(1, 4, 4)
-        self.polygon(
-            bins,
-            cumulative=True,
-            normalized=True,
-            title=f"Нормований полігон накопичених частот m={bins}",
-        )
-
-        plt.show()
-
-    def histogram(self, bins: int):
-        plt.hist(
-            self.data,
-            bins=bins,
-            label="$f_k$",
-            range=(self.min, self.max),
-            alpha=0.7,
-            edgecolor="black",
-        )
-        plt.title(f"Гістограма m={bins}")
-        plt.legend()
-
-    def polygon(
-        self, bins: int, title: str, cumulative: bool = False, normalized: bool = False
-    ):
-        frequency, bin_edges = np.histogram(self.data, bins=bins)
-        if normalized:
-            frequency = frequency / np.sum(frequency)
-        if cumulative:
-            frequency = np.cumsum(frequency)
-
-        bin_mids = (bin_edges[1:] + bin_edges[:-1]) / 2
-        plt.plot(bin_mids, frequency, marker="o", linestyle="-")
-        plt.title(title)
-        plt.xlabel("x")
-
-    def corridor(self, alpha=0.05):
-        sorted_data = np.sort(self.data)
-        n = len(sorted_data)
-
-        z_alpha = stats.norm.ppf(1 - alpha / 2)
-        ci = z_alpha / np.sqrt(n)
-
-        f = np.arange(1, n + 1) / n
-        lower_bound = f - ci
-        upper_bound = f + ci
-
-        plt.plot(
-            sorted_data,
-            lower_bound,
-            linestyle="--",
-            color="red",
-            label="$F_k - \\frac{z_{\\alpha}}{\\sqrt{n}}$",
-        )
-        plt.plot(
-            sorted_data,
-            upper_bound,
-            linestyle="--",
-            color="green",
-            label="$F_k + \\frac{z_{\\alpha}}{\\sqrt{n}}$",
-        )
-        plt.xlabel("x")
-        plt.legend()
-
-        plt.show()
-
-    def __str__(self):
-        return f"""
-            Максимальне значення: {self.max}\n
-            Мінімальне значення: {self.min}\n
-            Розмах вибірки: {self.range}\n
-            Кількість елементів: {self.n}\n
-            Середнє значення: {self.mean}\n
-            Дисперсія: {self.var}\n
-            Середнє квадратичне відхилення: {self.std}\n
-            Коефіцієнт асиметрії: {self.skew}\n
-            Коефіцієнт ексцесу: {self.kurtosis}\n
-        """
+def compute_statistics(data: np.ndarray) -> dict:
+    return {
+        "min": np.min(data),
+        "max": np.max(data),
+        "range": np.ptp(data),
+        "n": len(data),
+        "mean": np.mean(data),
+        "var": np.var(data),
+        "std": np.std(data),
+        "skew": stats.skew(data),
+        "kurtosis": stats.kurtosis(data),
+    }
 
 
-def N(z, eps=1e-3):
+def plot_histogram(data: np.ndarray, bins: int):
+    plt.hist(data, bins, alpha=0.7, edgecolor="black")
+    plt.title(f"Гістограма m={bins}")
+    plt.show()
+
+
+def plot_polygon(data: np.ndarray, bins: int, cumulative: bool = False, normalized: bool = False):
+    frequency, bin_edges = np.histogram(data, bins)
+    frequency = frequency / len(data) if normalized else frequency
+    frequency = np.cumsum(frequency) if cumulative else frequency
+
+    bin_mids = (bin_edges[1:] + bin_edges[:-1]) / 2
+    plt.plot(bin_mids, frequency, marker="o", linestyle="-")
+    plt.xlabel("x")
+    plt.show()
+
+
+def plot_corridor(data: np.ndarray, alpha: float = 0.05):
+    sorted_data, n = np.sort(data), len(data)
+    z_alpha, ci = stats.norm.ppf(1 - alpha / 2), stats.norm.ppf(1 - alpha / 2) / np.sqrt(n)
+
+    plt.plot(sorted_data, np.arange(1, n + 1) / n - ci, linestyle="--", color="red")
+    plt.plot(sorted_data, np.arange(1, n + 1) / n + ci, linestyle="--", color="green")
+    plt.xlabel("x")
+    plt.show()
+
+
+def find_N(z, eps=1e-3):
     return math.floor((1.0 / z) * np.sqrt(0.5 * np.log(1.0 / eps))) + 1
 
 
@@ -122,7 +53,7 @@ def K(z, eps=1e-3):
     if z <= 0:
         return 0
 
-    n = N(z, eps)
+    n = find_N(z, eps)
 
     def f(k):
         return (-1) ** k * np.exp(-2 * k**2 * z**2)
@@ -142,31 +73,31 @@ def plot(x, y, xlabel, ylabel, title, axhline=None):
     plt.show()
 
 
-if __name__ == "__main__":
-    np.random.seed(0)
-
-    data = np.random.normal(10, 9, 2002)
-    dist = SampleDistribution(data)
-
-    print(dist)
-
-    m = [10, 20]
-    for m1 in m:
-        dist.plot(m1)
-
-    dist.corridor()
-
+def plot_kolmogorov():
     z = np.linspace(0.1, 2, 500)
-
     eps = 1e-3
-
     k = [K(z1, eps) for z1 in z]
-
     alpha = 0.05
     plot(z, k, "z", "$K(z)$", "Критерій Колмогорова", axhline=1 - alpha)
-
-    n = [N(z1, eps) for z1 in z]
-
+    n = [find_N(z1, eps) for z1 in z]
     plot(z, n, "z", "$N(z)$", f"$N(z)$ при $\\varepsilon=${eps}")
 
-# %%
+
+def main():
+    np.random.seed(0)
+    A, B, N = 10, 9, 2002
+
+    data = np.random.normal(A, B, N)
+    print(compute_statistics(data))
+    for m in [10, 20]:
+        plot_histogram(data, m)
+        plot_polygon(data, m)
+        plot_polygon(data, m, cumulative=True)
+        plot_polygon(data, m, cumulative=True, normalized=True)
+
+    plot_corridor(data)
+    plot_kolmogorov()
+
+
+if __name__ == "__main__":
+    main()
