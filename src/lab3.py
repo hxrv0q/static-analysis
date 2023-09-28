9eab1028d88871e5b3c807a70a38fc12aa26c7ce
Ivan Tkachuk <116971836+seshotake@users.noreply.github.com>
2023-09-28 03:13:44 +0300

    Clean and refactor code

--- src/lab3.py
+++ src/lab3.py
@@ -1,186 +1,148 @@
-# %%
 import math
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 
 
-class NormalDistribution:
-    def __init__(self, data):
-        self.data = data
-        self.n = len(data)
-
-        self.mean = np.mean(data)
-        self.var = np.var(data, ddof=1)
-
-        assert np.equal(self.var, np.sum((data - self.mean) ** 2) / (self.n - 1))
-
-        # Ступені свободи
-        self.df = self.n - 1
-        # Стандартна похибка
-        self.se = self.var / np.sqrt(self.n)
-
-        self.alpha = 0.05
-        self.t = stats.t.ppf(1 - self.alpha, self.df)
-
-        self.ci_mean = [self.mean - self.t * self.se, self.mean + self.t * self.se]
-        self.chi2 = [
-            stats.chi2.ppf(self.alpha / 2, self.df),
-            stats.chi2.ppf(1 - self.alpha / 2, self.df),
-        ]
-        self.ci_var = [self.df * self.var**2 / chi for chi in self.chi2]
-
-    def __str__(self) -> str:
-        return f"""
-            границі довірчого інтервалу для мат. сподівання: {self.ci_mean}
-            границі довірчого інтервалу для дисперсії: {self.ci_var}
-            границі довірчого інтервалу xi-функції: {self.chi2}
-        """
-
-
-class PoissonDistribution:
-    def __init__(self, data, alpha=0.05):
-        self.data = data
+class ConfidenceIntervalAnalyzer:
+    def __init__(self, alpha=0.05):
         self.alpha = alpha
+        self.xalpha = stats.norm.ppf(1 - alpha / 2)
 
-        self.n = len(data)
+    def compute_cofidence_interval_normal(self, data):
+        N = len(data)
+        t = stats.t.ppf(1 - self.alpha, N - 1)
+        mx = np.mean(data)
+        Dx = np.var(data)
 
-        self.max = np.max(data)
-        self.min = np.min(data)
-
-        self.range = self.max - self.min
-
-        self.sorted_data = np.sort(data)
-
-    def histogram(self, bins: int = 10):
-        delta = self.range / bins
-        pk = np.array([self.min + delta / 2 * (2 * j - 1) for j in range(1, bins + 1)])
-
-        fk, _ = np.histogram(self.data, bins=np.linspace(self.min, self.max, bins + 1))
-        fk_normalized = fk / self.n
-
-        plt.bar(
-            pk,
-            fk_normalized,
-            width=delta,
-            align="center",
-            edgecolor="red",
-            facecolor="none",
+        ci_mx = (mx - t * Dx / np.sqrt(N), mx + t * Dx / np.sqrt(N))
+        ci_Dx = (
+            (N - 1) * Dx / stats.chi2.ppf(1 - self.alpha / 2, N - 1),
+            (N - 1) * Dx / stats.chi2.ppf(self.alpha / 2, N - 1),
         )
-        plt.xlabel("$p_k$")
-        plt.ylabel("$\\frac{f_k}{N}$")
+
+        return ci_mx, ci_Dx
+
+    def plot_histogram(self, data, bins, normalized=True):
+        pmin, pmax = np.min(data), np.max(data)
+        R = pmax - pmin
+        delta = R / bins
+
+        pk = np.array([pmin + delta / 2 * (2 * j - 1) for j in range(1, bins)])
+
+        fk, _ = np.histogram(data, bins=np.linspace(pmin, pmax, bins))
+        if normalized:
+            fk = fk / len(data)
+
+        plt.bar(pk, fk, width=delta, align="center", edgecolor="r", facecolor="none")
         plt.show()
 
-    def lam_est(self, n):
-        return 1 / n * np.sum(self.data[:n])
+    def estimate_lambda(self, data, n):
+        return 1 / n * np.sum(data[:n])
 
-    def lam_left(self, n, xalpha):
-        return (math.sqrt(self.lam_est(n)) - xalpha / (2 * math.sqrt(n))) ** 2
+    def compute_lambda_intervals(self, data, n_range):
+        lam_est = np.array([self.estimate_lambda(data, n) for n in n_range])
+        lam_left = np.array(
+            [
+                (math.sqrt(lam) - self.xalpha / (2 * math.sqrt(i))) ** 2
+                for lam, i in zip(lam_est, n_range)
+            ]
+        )
+        lam_right = np.array(
+            [
+                (math.sqrt(lam) + self.xalpha / (2 * math.sqrt(i))) ** 2
+                for lam, i in zip(lam_est, n_range)
+            ]
+        )
+        return lam_est, lam_left, lam_right
 
-    def lam_right(self, n, xalpha):
-        return (math.sqrt(self.lam_est(n)) + xalpha / (2 * math.sqrt(n))) ** 2
-
-    def plot_confidence_interval(self):
-        xalpha = stats.norm.ppf(1 - self.alpha / 2)
-        n = range(50, self.n + 1)
-
-        lam_est = np.array([self.lam_est(n) for n in n])
-        lam_left = np.array([self.lam_left(n, xalpha) for n in n])
-        lam_right = np.array([self.lam_right(n, xalpha) for n in n])
-
-        plt.plot(n, lam_est, label="$\\lambda est(n)$", color="r", linestyle="--")
-        plt.plot(n, lam_left, label="$\\lambda left(n)$", color="b", linestyle="--")
-        plt.plot(n, lam_right, label="$\\lambda right(n)$", color="g", linestyle="--")
-
-        plt.legend(loc="upper right")
+    def plot_lambda_intervals(self, n_range, lam_est, lam_left, lam_right):
+        plt.plot(n_range, lam_est, label="$\\lambda est(n)$", color="r", linestyle="--")
+        plt.plot(
+            n_range, lam_left, label="$\\lambda left(n)$", color="g", linestyle="--"
+        )
+        plt.plot(
+            n_range, lam_right, label="$\\lambda right(n)$", color="b", linestyle="--"
+        )
+        plt.legend()
         plt.xlabel("$n$")
         plt.show()
 
-        lam_diff = lam_right - lam_left
+        lam = lam_right - lam_left
         plt.plot(
-            n,
-            lam_diff,
-            label="$\\lambda right(n) - \\lambda left(n)$",
-            color="r",
+            n_range, lam, label="$\\lambda right(n) - \\lambda left(n)$", color="r"
         )
-        plt.legend(loc="upper right")
+        plt.legend()
         plt.show()
 
+    def compute_and_plot_correlation_interval(self, xy):
+        n = len(xy[0])
+        xmean, ymean = np.mean(xy, axis=1)
+        m = 1 / n * np.sum((xy[0] - xmean) * (xy[1] - ymean))
 
-def cofidence_interval(xy: list[int, 2], alpha=0.05):
-    xalpha = stats.norm.ppf(1 - alpha / 2)
+        sigma2x = 1 / n * np.sum((xy[0] - xmean) ** 2)
+        sigma2y = 1 / n * np.sum((xy[1] - ymean) ** 2)
+        k = m / math.sqrt(sigma2x * sigma2y)
 
-    n = len(xy[0])
+        k_formula = (
+            np.sum(xy[0] * xy[1]) - 1 / n * (np.sum(xy[0]) * np.sum(xy[1]))
+        ) / math.sqrt(
+            (np.sum(xy[0] ** 2) - 1 / n * (np.sum(xy[0])) ** 2)
+            * (np.sum(xy[1] ** 2) - 1 / n * np.sum(xy[1]) ** 2)
+        )
 
-    xmean = np.mean(xy[0])
-    ymean = np.mean(xy[1])
+        assert abs(k - k_formula) < 1e-5
 
-    m = 1 / n * np.sum((xy[0] - xmean) * (xy[1] - ymean))
+        kleft = math.tanh(math.atanh(k) - self.xalpha / math.sqrt(n - 3))
+        kright = math.tanh(math.atanh(k) + self.xalpha / math.sqrt(n - 3))
 
-    sigma2x = 1 / n * np.sum((xy[0] - xmean) ** 2)
-    sigma2y = 1 / n * np.sum((xy[1] - ymean) ** 2)
+        plt.scatter(xy[0], xy[1], color="r")
+        plt.show()
 
-    k = m / math.sqrt(sigma2x * sigma2y)
-
-    k_formula = (
-        np.sum(xy[0] * xy[1]) - 1 / n * (np.sum(xy[0]) * np.sum(xy[1]))
-    ) / math.sqrt(
-        (np.sum(xy[0] ** 2) - 1 / n * (np.sum(xy[0])) ** 2)
-        * (np.sum(xy[1] ** 2) - 1 / n * (np.sum(xy[1])) ** 2)
-    )
-
-    assert abs(k - k_formula) < 1e-5
-
-    kleft = math.tanh(math.atanh(k) - xalpha / np.sqrt(n - 3))
-    kright = math.tanh(math.atanh(k) + xalpha / np.sqrt(n - 3))
-
-    print(f"Коефіцієнт кореляції: {k}")
-    print(f"Границі довірчого інтервалу: [{kleft}, {kright}]")
-
-    plt.plot(xy[0], xy[1], "o", color="red", label="$[(XY^T)^{<2>}]$")
-    plt.ylabel("$[(XY^T)^{<2>}]$")
-    plt.xlabel("$[(XY^T)^{<1>}]$")
-    plt.legend()
-    plt.show()
+        return kleft, k, kright
 
 
 def main():
+    analyzer = ConfidenceIntervalAnalyzer()
+
+    A, B, N = 10, 9, 2002
+
     np.random.seed(0)
 
-    data = np.random.normal(10, 9, 2002)
+    normal_data = np.random.normal(A, B, N)
+    ci_mx, ci_var = analyzer.compute_cofidence_interval_normal(normal_data)
+    print(f"Довірчий інтервал для мат. сподівання: {ci_mx}")
+    print(f"Довірчий інтервал для дисперсії: {ci_var}")
 
-    normal = NormalDistribution(data)
-
-    print(normal)
-
+    # Розподіл Пуассона
     lam = 6
-    data = np.random.poisson(lam, 2002)
+    poisson_data = np.random.poisson(lam, N)
+    analyzer.plot_histogram(poisson_data, 10)
+    n_range = range(50, N)
+    l_est, l_left, l_right = analyzer.compute_lambda_intervals(poisson_data, n_range)
+    analyzer.plot_lambda_intervals(n_range, l_est, l_left, l_right)
 
-    poisson = PoissonDistribution(data)
-
-    poisson.histogram(bins=10)
-    poisson.plot_confidence_interval()
-
-    xy1 = np.array(
+    # Коефіцієнт кореляції
+    xy = np.array(
         [
             [-2.7, -0.931, -0.257, 1.381, -0.315, -3.05, 0.054, 0.835],
             [-14.902, -18.113, 6.138, 13.813, -0.227, 4.927, 2.576, 1.184],
         ]
     )
-    print(xy1)
-    cofidence_interval(xy1)
+    kleft, k, kright = analyzer.compute_and_plot_correlation_interval(xy)
+    print(f"Коефіцієнт кореляції: {k}")
+    print(f"Границі довірчого інтервалу: [{kleft}, {kright}]")
 
-    xy2 = np.array(
+    xy = np.array(
         [
             [1.661, 3.333, -1.12, 0.377, -2.28, -5.092, 3.124],
             [-14.433, 1.527, 11.866, 2.121, -6.254, 1.58, 13.972],
         ]
     )
-    print(xy2)
-    cofidence_interval(xy2)
+    kleft, k, kright = analyzer.compute_and_plot_correlation_interval(xy)
+    print(f"Коефіцієнт кореляції: {k}")
+    print(f"Границі довірчого інтервалу: [{kleft}, {kright}]")
 
 
 if __name__ == "__main__":
     main()
-
-# %%
